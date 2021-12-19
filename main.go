547e6fa887ebde4d0366386efdb96dff5d6ca0d0
PeaWarrior <jacksonchen171@gmail.com>
2021-12-19 17:24:45 -0500

    add deck type with print

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("hello world")
+	cards := deck{"Ace of Diamonds", "King of Diamonds"}
+	cards = append(cards, "Queen of Diamonds")
+
+	cards.print()
 }
